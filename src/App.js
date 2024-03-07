60f8ad917185d76d2fbcf35273fbb2143226db1f
Live-25 <liveleen25@gmail.com>
2024-03-07 22:46:50 +0530

    Merge branch 'main' of https://github.com/Live-25/Portfolio-live into new_changes

--- src/App.js
+++ src/App.js
@@ -6,27 +6,33 @@ import Technology from './components/Technology';
 import Project from './components/Project';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-//import {HashRouter as Router , Routes , Route} from 'react-router-dom';
+import {BrowserRouter , Routes , Route} from 'react-router-dom';
+/* import {  } from 'react-router-dom';
 
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+// ...
+
+<Router>
+ 
+</Router> */
+
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/technology" element={<Technology />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/projects" element={<Project />} />
-        </Routes>
-      </div>
-    </Router>
+    <BrowserRouter>
+    <div className="App">
+    <Routes>
+   
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/technology" element={<Technology />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/projects" element={<Project />} />
+    </Routes>
+
+     
+    </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
-
-
